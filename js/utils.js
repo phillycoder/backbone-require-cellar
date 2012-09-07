54813f6076aca5627251271facd515cae3723da6
Mustaq <jmustaq@gmail.com>
2012-09-07 15:14:31 -0400

    cleanup

--- js/utils.js
+++ js/utils.js
@@ -7,24 +7,6 @@ define([
 
 var utils = {
 
-    // Asynchronously load templates located in separate .html files
-    loadTemplate: function(views, callback) {
-
-        var deferreds = [];
-
-        $.each(views, function(index, view) {
-            if (window[view]) {
-                deferreds.push($.get('tpl/' + view + '.html', function(data) {
-                    window[view].prototype.template = _.template(data);
-                }));
-            } else {
-                alert(view + " not found");
-            }
-        });
-
-        $.when.apply(null, deferreds).done(callback);
-    },
-
     uploadFile: function (file, callbackSuccess) {
         var self = this;
         var data = new FormData();
